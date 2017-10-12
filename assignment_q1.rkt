f505bcee28ff33a5fb2a4845a3a4fdae2718a189
Liam O'Neill <l.oneill20@nuigalway.ie>
2017-10-12 14:15:42 +0100

    Last three functions for lists in question 1.

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,7 +1,16 @@
 #lang racket
 
-(define (pair_numbers x y)
-  (cons x y))
+(define (pair_numbers num1 num2)
+  (cons num1 num2))
 
-(define (three_list x y z)
-  (cons x (cons y (cons z '()))))
+(define (three_list num1 num2 num3)
+  (cons num1 (cons num2 (cons num3 '()))))
+
+(define (list_with_cons str num nested_list)
+  (cons str (cons num (cons nested_list '()))))
+
+(define (list_with_list str num nested_list)
+  (list str num nested_list))
+
+(define (list_with_append str num nested_list)
+  (append (str) (num) (nested_list)))
