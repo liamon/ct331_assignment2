4c10cec9eed50b59c665bc6f95bdd1754e0773b1
Liam O'Neill <l.oneill20@nuigalway.ie>
2017-10-12 15:05:05 +0100

    ins_end in question 2.

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -5,3 +5,9 @@
 ; elements of lst onto the end of this new list.
 (define (ins_beg el lst)
   (append (list el) lst))
+
+(provide ins_end)
+; This makes a new list with only one element, el, and then appends the
+; single element in this new list onto the end of lst.
+(define (ins_end el lst)
+  (append lst (list el)))
