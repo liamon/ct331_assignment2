350986f5868f48cbe159a28ff8e4a2e0b5c48812
Liam O'Neill <l.oneill20@nuigalway.ie>
2017-10-12 19:14:09 +0100

    Added a function for test results, like in the C assignment.

--- tests.rkt
+++ tests.rkt
@@ -10,11 +10,17 @@
   (begin
     (display "Running tests...\n")
     ;begin calling test functions
-    (printf "1: ~a" (test_ins_beg_atom))
+    (display (test_result_message test_ins_beg_atom "Inserting an atom at the start of a list"))
+    (display (test_result_message test_ins_beg_list "Inserting a list at the start of a list"))
     ;end calling test functions
     (display "\nTests complete!\n")))
 
 ;Begin test functions
+(define (test_result_message test_function test_description)
+  (string-append test_description ": " (if (test_function)
+                                       "SUCCESS"
+                                       "FAILURE") "\n"))
+
 (define (test_ins_beg_atom) ; eq? checks if they refer to the same object
   (equal? '(1 2 3 4) (ins_beg 1 '(2 3 4)))) ; so I need to use equal?
 
