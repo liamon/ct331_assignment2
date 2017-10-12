8c9b0c3ae929283ccb0b0885887bde83129b6974
Liam O'Neill <l.oneill20@nuigalway.ie>
2017-10-12 15:16:58 +0100

    count_top_level in question 2.

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -11,3 +11,12 @@
 ; single element in this new list onto the end of lst.
 (define (ins_end el lst)
   (append lst (list el)))
+
+(provide count_top_level)
+; This is a recursive function. If the list is empty, then the number of
+; items in the list is zero. Otherwise, the number is one plus the number
+; of items in a list one smaller than this list.
+(define (count_top_level lst)
+  (if (null? lst)
+      0
+      (+ 1 ( count_top_level (cdr lst)))))
