9463ce1e222729b3d41628c6b88c34dd6fdf0ddf
Liam O'Neill <l.oneill20@nuigalway.ie>
2017-10-12 15:50:51 +0100

    Started adding tests for ins_beg.

--- tests.rkt
+++ tests.rkt
@@ -3,10 +3,6 @@
 (require (file "assignment_q2.rkt")
          (file "assignment_q3.rkt"))
 
-;
-;Don't worry about this file unless you are doing the extra credit tests. 
-;
-
 ;This structure allows a single function call
 ;to run every test in sequence, rather than
 ;calling each function separately. 
@@ -14,13 +10,16 @@
   (begin
     (display "Running tests...\n")
     ;begin calling test functions
-    (printf "1: ~a" (test_ins_beg1))
+    (printf "1: ~a" (test_ins_beg_atom))
     ;end calling test functions
     (display "\nTests complete!\n")))
 
 ;Begin test functions
-(define (test_ins_beg1)
+(define (test_ins_beg_atom)
   (eq? (ins_beg 1 '(2 3 4)) '(1 2 3 4)))
+
+(define (test_ins_beg_list)
+  (eq? (ins_beg '(1 6) '(2 3 4)) '((1 6) 2 3 4)))
 
 ;End test functions
 
