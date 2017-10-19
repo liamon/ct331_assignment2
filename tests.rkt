7f15809408a602d83560c750cbbba8abf4e53d9c
Liam O'Neill <l.oneill20@nuigalway.ie>
2017-10-19 10:32:34 +0100

    Added tests for count_instances_deep.

--- tests.rkt
+++ tests.rkt
@@ -21,8 +21,17 @@
 
     (display (test_result_message test_count_instances_absent "Counting instances of an item absent from a list"))
     (display (test_result_message test_count_instances_present "Counting instances of an item present in a list"))
-    (display (test_result_message test_count_instances_tr_absent "Counting instances of an item absent from a list with tail-recursion"))
-    (display (test_result_message test_count_instances_tr_present "Counting instances of an item present in a list with tail-recursion"))
+    (display (test_result_message test_count_instances_tr_absent
+                                  "Counting instances of an item absent from a list with tail-recursion"))
+    (display (test_result_message test_count_instances_tr_present
+                                  "Counting instances of an item present in a list with tail-recursion"))
+
+    (display (test_result_message test_count_instances_deep_absent "Deep counting instances of an item absent from a list"))
+    (display (test_result_message test_count_instances_deep_present "Deep counting instances of an item present in a list"))
+    (display (test_result_message test_count_instances_deep_absent_sublists
+                                  "Deep counting instances of an item absent from a list with sublists"))
+    (display (test_result_message test_count_instances_deep_present_sublists
+                                  "Deep counting instances of an item present in a list with sublists"))
     ;end calling test functions
     
     (display "\nTests complete!\n")))
@@ -65,6 +74,18 @@
 
 (define (test_count_instances_tr_present)
   (= 2 (count_instances_tr 1 '(0 2 1 9 1))))
+
+(define (test_count_instances_deep_absent)
+  (= 0 (count_instances_tr 3 '(0 1 55))))
+
+(define (test_count_instances_deep_present)
+  (= 2 (count_instances_tr 1 '(0 2 1 9 1))))
+
+(define (test_count_instances_deep_absent_sublists)
+  (= 0 (count_instances_tr 3 '((0 1) 55))))
+
+(define (test_count_instances_deep_present_sublists)
+  (= 2 (count_instances_tr 1 '(0 (2 1) (9) 1))))
 
 ;End test functions
 
