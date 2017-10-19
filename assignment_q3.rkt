963ea1b15c4431be93aea0a7e54b4a63d99c7801
Liam O'Neill <l.oneill20@nuigalway.ie>
2017-10-19 15:33:33 +0100

    Multiple changes, including fixing insert.

--- assignment_q3.rkt
+++ assignment_q3.rkt
@@ -2,20 +2,22 @@
 
 (define example_tree '(((() 5 ()) 9 (() 17 ())) 28 ((() 33 ()) 40 (() 45 ()))))
 
-(define (display_sorted tree)
-  (cond [(empty? (get_left_child tree)) (display (value tree))]))
+(define (display_sorted tree) ; TODO
+  (cond [(empty? (left_child tree)) (display (value tree))]))
 
 (define (present_in_tree item tree)
   (cond [(empty? tree) #f]
         [(equal? item (value tree)) #t]
-        [(< item (value tree)) (present_in_tree item (get_left_child tree))]
-        [else (present_in_tree item (get_right_child tree))])) ; (> item tree_value)
+        [(< item (value tree)) (present_in_tree item (left_child tree))]
+        [else (present_in_tree item (right_child tree))])) ; (> item tree_value)
 
-(define (insert item tree) ; TODO This should return a tree, I think.
-  (cond [(empty? tree) '(() item ())]
-        [(equal? item (value tree)) '()]
-        [(< item (value tree)) (insert item (get_left_child tree))]
-        [else (insert item (get_right_child tree))]))
+(define (insert item tree)
+  (cond [(empty? tree) (list '() item '())]
+        [(equal? item (value tree)) tree]
+        [(< item (value tree))
+         (list (insert item (left_child tree)) (value tree) (right_child tree))]
+        [else ; (> item (value tree)
+         (list (left_child tree) (value tree) (insert item (right_child tree)))]))
 
 (define (insert_list lst tree)
   (if (empty? lst) tree
@@ -30,11 +32,13 @@
 (define (value tree)
   (cadr tree))
 
-(define (get_left_child tree)
+(define (left_child tree)
   (car tree))
 
-(define (get_right_child tree)
+(define (right_child tree)
   (caddr tree)) ; Can't say (cddr tree), as that would return the right child put inside a list.
 
 (present_in_tree 40 example_tree)
 (present_in_tree 109 example_tree)
+
+(insert 8 example_tree)
